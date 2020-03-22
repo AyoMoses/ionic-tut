39a870ea3ce0d44477585d82f84dfbb702a3a960
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-03-22 22:58:48 +0100

    extracting and displaying route param in pages

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipesService } from '../recipes.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.page.html',
-  styleUrls: ['./recipe-detail.page.scss'],
+  styleUrls: ['./recipe-detail.page.scss']
 })
 export class RecipeDetailPage implements OnInit {
+  // TO USE IN TEMPLATE, WE STORE IN A PROPERTY OF OUR CLASS OF LOADEDRECIPE
+  // IT IS A GOOD PRACTICE TO STATE THE TYPE OF PROP AN OBJECT WILL HOLD
+  // WE IMPORT RECIPE FROM RECIPE.MODEL
+  loadedRecipe: Recipe;
 
-  constructor() { }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipesService: RecipesService
+  ) {}
 
   ngOnInit() {
+    // paramMap means a map of all the parameters this route receives
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('recipeId')) {
+        // redirect
+        return;
+      }
+      // name has to match what is in the routing config
+      const recipeId = paramMap.get('recipeId');
+      // WE THEN STORE THE RECIPE BEING RETRIEVED
+      this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+    });
   }
-
 }
